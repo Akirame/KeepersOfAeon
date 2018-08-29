a016cc073b3f0b39e4667e8c8c91fe2a8f1c723c
Akirame <gsvillalba12@hotmail.com>
2018-08-29 19:00:39 -0300

    Character Controll & Door

---
+++ ZemeaTD/Assets/Game/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject ObjectiveToGo;
+
+    public void GotoObjective(GameObject go)
+    {
+        go.transform.position = ObjectiveToGo.transform.position;
+    }
+}
