a01e278fefe9f5aaaaee027fe9c87086e7f40c56
Akirame <gsvillalba12@hotmail.com>
2018-09-07 14:35:38 -0300

    Fixed Flyer Enemy

--- ZemeaTD/Assets/Game/Scripts/Enemy/EnemyFlyerBehaviour.cs
+++ ZemeaTD/Assets/Game/Scripts/Enemy/EnemyFlyerBehaviour.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyFlyerBehaviour : MonoBehaviour
 {
-    private Rigidbody2D rig;
+    private Rigidbody2D rig;    
+    public float time = 3;
+    public float maxY = 200;
+    private float timer = 0;
+
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.gravityScale = 0;
-        transform.position = new Vector2(transform.position.x, 20);
+        transform.position = new Vector2(transform.position.x, 20);        
     }
     private void Update()
     {
-        
+        if (timer < time)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, maxY * Time.deltaTime);
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            maxY *= -1;
+        }
     }
 }
