dba25b5294590907d4d8d28e6e65ea8694d7bd84
Unknown <dani_mrx93@hotmail.com>
2018-09-17 22:14:02 -0300

    Add bouncing platform

---
+++ ZemeaTD/Assets/Game/Scripts/Trampolin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampolin : MonoBehaviour {
+
+    public float jumpForce;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (Input.GetKeyDown(collision.gameObject.GetComponent<InputControl>().jump))
+            {
+                Rigidbody2D rig = collision.gameObject.GetComponent<Rigidbody2D>();
+                rig.velocity = new Vector2(rig.velocity.x, jumpForce);
+            }
+        }
+    }
+
+}
