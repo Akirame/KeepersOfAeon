2f855224f24e1e39eaf9020d5b391241351c104b
Akirame <gsvillalba12@hotmail.com>
2018-09-26 20:38:05 -0300

    Rework Spawner

--- ZemeaTD/Assets/Game/Scripts/Enemy/EnemyManager.cs
+++ ZemeaTD/Assets/Game/Scripts/Enemy/EnemyManager.cs
@@ -2,110 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
-{
+public class EnemyManager : MonoBehaviour {
     #region singleton
     private static EnemyManager instance;
-    public static EnemyManager GetInstance()
-    {
+    public static EnemyManager GetInstance() {
         return instance;
     }
-    private void Awake()
-    {
-        if (!instance)
+    private void Awake() {
+        if(!instance)
             instance = this;
         else
             Destroy(this.gameObject);
     }
     #endregion
 
-    [System.Serializable]
-    public class EnemyList
-    {
-        public List<Enemy> enemies;
-    }
-    public List<EnemyList> EnemyWaves;
     public Transform[] spawnPoints;
     public GameObject wave;
+    public int minCantMelee = 5;
+    public int minCantRanged = 1;
+    public int minCantFlyer = 1;
+    public List<Enemy> TypeOfEnemies;
+    public float timeBetweenWaves;
+    public float timeBetweenEnemies;
+    private List<int> enemiesIndex;
+    private List<Enemy> enemies;
     private int currentWave;
+    private int enemyMultiplier;
     private int currentEnemies;
     private int enemyConta;
-    private float timeBetweenWaves;
-    private float timeBetweenEnemies;
+    private int totalEnemiesToSpawn;
     private float timer;
-    private bool nextWave;    
+    private bool nextWave;
+    private bool waveCalculated;
 
-    private void Start()
-    {
+    private void Start() {
         Enemy.Death += EnemyKilled;
         nextWave = true;
         currentWave = 1;
         wave = new GameObject();
-        timeBetweenWaves = 5;
-        timeBetweenEnemies = 1.5f;
         enemyConta = 0;
         timer = 0;
-        currentEnemies = 0;        
+        currentEnemies = 0;
+        waveCalculated = false;
+        enemiesIndex = new List<int>();
+        enemies = new List<Enemy>();
+        enemyMultiplier = 0;
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Enemy e = EnemyWaves[0].enemies[Random.Range(0, EnemyWaves[0].enemies.Count)];
-                Transform t = spawnPoints[Random.Range(0, spawnPoints.Length)];
-                Instantiate(e.transform.gameObject, t.position, Quaternion.identity, wave.transform);
-            }
+    private void Update() {
+        if(nextWave) {
+            SpawnWave();
         }
-        if (currentWave <= EnemyWaves.Count)
-        {
-            if (nextWave)
-            {
-                SpawnWave();
+        if(currentEnemies <= 0) {
+            if(timer >= timeBetweenWaves) {
+                currentWave++;
+                nextWave = true;
+                CleanWave();
+                timer = 0;
             }
-            if (currentEnemies <= 0)
-            {
-                if (timer >= timeBetweenWaves)
-                {
-                    currentWave++;
-                    nextWave = true;
-                    timer = 0;
-                }
-                else
-                    timer += Time.deltaTime;
-            }
+            else
+                timer += Time.deltaTime;
         }
     }
-    private void SpawnWave()
-    {
+    private void CalculateWave() {
+        for(int i = 0; i < minCantMelee + enemyMultiplier; i++) {
+            enemiesIndex.Add(0);
+        }
+        for(int i = 0; i < minCantRanged + (int)enemyMultiplier / 3; i++) {
+            enemiesIndex.Add(1);
+        }
+        for(int i = 0; i < minCantFlyer + (int)enemyMultiplier / 5; i++) {
+            enemiesIndex.Add(2);
+        }
+        enemyConta = enemiesIndex.Count;
+        for(int i = 0; i < enemyConta; i++) {
+            int randomIndexNumber = Random.Range(0, enemiesIndex.Count);
+            enemies.Add(TypeOfEnemies[enemiesIndex[randomIndexNumber]]);
+            enemiesIndex.RemoveAt(randomIndexNumber);
+        }
+        totalEnemiesToSpawn = enemies.Count;
+        waveCalculated = true;
+        enemyConta = 0;
+        enemyMultiplier++;
+
+    }
+    private void SpawnWave() {
         wave.name = "Wave " + currentWave;
-        if ((enemyConta < EnemyWaves[currentWave - 1].enemies.Count))
-        {
-            if (timer >= timeBetweenEnemies)
-            {
-                Enemy e = EnemyWaves[currentWave - 1].enemies[enemyConta];
+        if(!waveCalculated)
+            CalculateWave();
+        if((enemyConta < totalEnemiesToSpawn)) {
+            if(timer >= timeBetweenEnemies) {
+                Enemy e = enemies[enemyConta];
                 Transform t = spawnPoints[Random.Range(0, spawnPoints.Length)];
                 Instantiate(e.transform.gameObject, t.position, Quaternion.identity, wave.transform);
                 enemyConta++;
                 timer = 0;
             }
-            else
-            {
+            else {
                 timer += Time.deltaTime;
             }
         }
-        else
-        {
+        else {
             currentEnemies = enemyConta;
             nextWave = false;
+            waveCalculated = false;
             enemyConta = 0;
             timer = 0;
         }
 
     }
+    private void CleanWave() {
+        enemies.Clear();        
+        enemiesIndex.Clear();        
+    }
     private void EnemyKilled(Enemy e)
-    {
+    {        
         Destroy(e.gameObject);
         currentEnemies--;
     }
