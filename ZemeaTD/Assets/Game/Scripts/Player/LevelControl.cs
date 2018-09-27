efc729f4173d8cf3dcc4a6b29c57a3861bdabf80
Unknown <dani_mrx93@hotmail.com>
2018-09-26 23:00:29 -0300

    Add LevelControl

---
+++ ZemeaTD/Assets/Game/Scripts/Player/LevelControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour {
+
+    public int playerLevel = 1;
+    public float playerExperience = 0;
+    public int expNeededPerLevel = 300;
+}
