c57ce5cb5d2e12258412874fe6a04cad31108466
Unknown <dani_mrx93@hotmail.com>
2018-09-27 22:21:07 -0300

    Fix PlayerLevel behaviour

---
+++ ZemeaTD/Assets/Game/Scripts/Player/PlayerLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+
+    public int playerLevel = 1;
+    public int playerExperience = 0;
+    public int expNeededPerLevel = 300;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            playerLevel++;
+            print("DEBUG - LEVEL UP - PlayerLevel.cs");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            AddExperience(500);
+            print("DEBUG - ADD EXPERIENCE - PlayerLevel.cs");
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        playerExperience += amount;
+        while (playerExperience >= playerLevel * expNeededPerLevel)
+        {
+            playerExperience -= playerLevel * expNeededPerLevel;
+            playerLevel++;
+        }
+    }
+
+    public void LevelUpTo(int level)
+    {
+        playerLevel = level;
+    }
+}
--- ZemeaTD/Assets/Game/Scripts/UIManager.cs
+++ ZemeaTD/Assets/Game/Scripts/UIManager.cs
@@ -6,12 +6,34 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Text waveText;
+    public Text P1Level;
+    public Text P2Level;
     public Image orbPlayer1;
     public Image orbPlayer2;
+    public Image expBarP1;
+    public Image expBarP2;
     public AttackBehaviour player1;
     public AttackBehaviour player2;
     public Sprite[] orbSprites; 
     private string waveTextAux;
+    private PlayerLevel p1;
+    private PlayerLevel p2;
+    private int p1Level;
+    private int p2Level;
+    private int p1Exp;
+    private int p2Exp;
+
+    private void Start()
+    {
+        p1 = player1.gameObject.GetComponent<PlayerLevel>();
+        p2 = player2.gameObject.GetComponent<PlayerLevel>();
+        p1Level = p1.playerLevel;
+        p2Level = p2.playerLevel;
+        p1Exp = p1.playerExperience;
+        p2Exp = p2.playerExperience;
+        expBarP1.fillAmount = p1.playerExperience / (p1.expNeededPerLevel * p1.playerLevel);
+        expBarP2.fillAmount = p2.playerExperience / (p2.expNeededPerLevel * p2.playerLevel);
+    }
 
     private void Update()
     {
@@ -21,6 +43,31 @@ public class UIManager : MonoBehaviour
             waveTextAux = EnemyManager.GetInstance().wave.name;            
         }
         ChangeOrbImage();
+
+
+        if (p1Exp != p1.playerExperience)
+        {
+            p1Exp = p1.playerExperience;
+            expBarP1.fillAmount = (float)p1.playerExperience/ (p1.expNeededPerLevel * p1.playerLevel);
+        }
+
+        if (p2Exp != p2.playerExperience)
+        {
+            p2Exp = p2.playerExperience;
+            expBarP2.fillAmount = (float)p2.playerExperience / (p2.expNeededPerLevel * p2.playerLevel);
+        }
+
+        if (p1.playerLevel != p1Level)
+        {
+            p1Level = p1.playerLevel;
+            P1Level.text = "Lvl " + p1.playerLevel.ToString();
+        }
+        if (p2.playerLevel != p2Level)
+        {
+            p2Level = p2.playerLevel;
+            P2Level.text = "Lvl " + p2.playerLevel.ToString();
+        }
+
     }
     
     private void ChangeOrbImage()//delegate
