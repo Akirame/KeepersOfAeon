6a17df861457d5ff07467454452e9e716e40b0c8
Unknown <dani_mrx93@hotmail.com>
2018-10-18 22:56:14 -0300

    Rework PopText

--- ZemeaTD/Assets/Game/Scripts/HUD/PopText.cs
+++ ZemeaTD/Assets/Game/Scripts/HUD/PopText.cs
@@ -1,14 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PopText : MonoBehaviour {
 
     public TextMesh popText;
+    public int alphaTime;
+    private Vector3 finalPos;
+    private float timer;
+    private TextMesh text;
+    private float alphaDecrementer = 1;
 
     private void Start()
     {
         popText = GetComponent<TextMesh>();
+        text = GetComponentInChildren<TextMesh>();
+        finalPos.x = Random.Range(-5, 5);
+        finalPos.y = 10;
+        finalPos += transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, finalPos, 1);
+        timer += Time.deltaTime;
+        if (timer > alphaTime)
+        {
+            alphaDecrementer -= Time.deltaTime;
+            text.color = new Color(1, 1, 1, alphaDecrementer);
+            transform.localScale += new Vector3(1f, 1f, 1f) * Time.deltaTime;
+        }
+        if (text.color.a <= 0.1)
+        {
+            DestroyText();
+        }
     }
 
     public void CreateText(string text, Color colorText)
