2c2c88e76a7f7c0ebc0964baa18ece9dafc212d0
Unknown <dani_mrx93@hotmail.com>
2018-11-04 18:23:42 -0300

    Add target to start game

---
+++ ZemeaTD/Assets/Game/Scripts/Tutorial/Target.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public WaveControl waveControl;
+    public GameObject tutorial;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            if (!waveControl.gameStarted)
+            {
+                waveControl.gameStarted = true;
+                TutorialEnd();
+            }
+        }
+    }
+
+    private void TutorialEnd()
+    {
+        foreach (Transform item in tutorial.GetComponentsInChildren<Transform>())
+        {
+            Destroy(item.gameObject);
+        }
+    }
+}
