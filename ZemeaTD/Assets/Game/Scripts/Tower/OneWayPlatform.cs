cd38afa64eddec14e848024f1a7c5663a124bda6
Akirame <gsvillalba12@hotmail.com>
2018-11-20 23:27:45 -0300

    Add coroutine to ignoreCollision

--- ZemeaTD/Assets/Game/Scripts/Tower/OneWayPlatform.cs
+++ ZemeaTD/Assets/Game/Scripts/Tower/OneWayPlatform.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class OneWayPlatform : MonoBehaviour {
 
-    private PlatformEffector2D effector;
-    private List<Collider2D> playerCollision;
-
+    private List<Collider2D> playerCollision; 
 	// Use this for initialization
-	void Start () {
-        effector = GetComponent<PlatformEffector2D>();        
+	void Start () { 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
+    IEnumerator CancelIgnore(Collider2D playerCollider)
+    {
+        yield return new WaitForSeconds(0.2f);
+        Physics2D.IgnoreCollision(playerCollider, GetComponent<BoxCollider2D>(),false);
+    }
     public void Deactivate(Collider2D playerCollider)
     {
         Physics2D.IgnoreCollision(playerCollider, GetComponent<BoxCollider2D>());
+        StartCoroutine(CancelIgnore(playerCollider));
     }
 
 }
